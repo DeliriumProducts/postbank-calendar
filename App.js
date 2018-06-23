723c596c573694c4e000f50a4a2a0034969f12a9
Simo Aleksandrov <simo3003@me.com>
2018-06-24 01:15:36 +0300

    Add calendar and view in App component

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Calendar />
       </View>
     );
   }
@@ -19,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: '50%'
   },
 });
