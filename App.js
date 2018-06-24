e602d08421bcf631c0953df2f90e62c19fc60a80
Simo Aleksandrov <simo3003@me.com>
2018-06-24 03:51:37 +0300

    Remove material-design component

--- App.js
+++ App.js
@@ -5,18 +5,12 @@ import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Calendar />
-      </View>
+        <Agenda/>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#4286f4',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
