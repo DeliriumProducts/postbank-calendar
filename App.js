8f56661790fdacd32b733c645f3deb1639750021
Simo Aleksandrov <simo3003@me.com>
2018-06-24 05:33:33 +0300

    Add bg language locale

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { StyleSheet, Text, View, Navigator, NativeModules, Button } from 'react-native';
+import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
+
+LocaleConfig.locales['bg'] = {
+  monthNames: ['Януари','Февруари','Март','Април','Май','Юни','Юли','Август','Септември','Октомври','Ноември','Декември'],
+  monthNamesShort: ['Ян.','Фев.','Март','Апр.','Май','Юни','Юли','Авг.','Сеп.','Окт.','Ноем.','Дек.'],
+  dayNames: ['Неделя','Понеделник','Вторник','Сряда','Четвъртък','Петък','Събота'],
+  dayNamesShort: ['Нед.','Пон.','Вт.','Ср.','Чт.','Пт.','Сб.']
+};
+LocaleConfig.defaultLocale = 'bg';
 
 export default class App extends React.Component {
   render() {
     return (
-        <Agenda/>
+     <Agenda/>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
